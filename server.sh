4876942d1a28e538a47122c1d11feb6f36abbb5f
shyam-habarakada <shyam.habarakada@gmail.com>
2012-10-07 11:24:47 -0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -8,8 +8,11 @@ var host = "dev.artefactgroup.com",
     port = "8088",
     thisServerUrl = "http://" + host + ":" + port;
 
+/* The sites that are being continuously integrated.
+ * Add new site definition here and specify the updateCmd to be run when a post recieve hook is invoked
+ */
 var siteMap = {
-    wwwstage : { updateCmd : "cd /var/www-benchpress-mkt-site-stage; git pull origin master" }
+    wwwstage : { updateCmd : "cd /var/www-benchpress-mkt-site-stage; git pull origin master; mail you@yourdomain.com - s 'wwwStage updated at `date`'" }
   }
 
 http.createServer(function (req, res) {
